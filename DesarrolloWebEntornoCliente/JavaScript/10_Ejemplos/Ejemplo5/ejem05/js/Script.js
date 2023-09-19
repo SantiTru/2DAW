3dc65491bfdc6b7b59d6a3fb32a77ec6a468dc22
SantiTru <santiagotrujillonavarro@gmail.com>
2023-09-19 18:37:40 +0200

    Commit

--- DesarrolloWebEntornoCliente/JavaScript/10_Ejemplos/Ejemplo5/ejem05/js/Script.js
+++ DesarrolloWebEntornoCliente/JavaScript/10_Ejemplos/Ejemplo5/ejem05/js/Script.js
@@ -1,11 +1,5 @@
-"use strict"
-
 let person = prompt ("Please enter your name", "Por ejemplo, 'Dumbeldor'");
 
 function saludar(){
   alert("Hola" + person);
-}
-
-function despedir(){
-  alert("Adios" + person);
 }
---
+++ DesarrolloWebEntornoCliente/JavaScript/10_Ejemplos/Ejemplo5/ejem06/js/script.js
@@ -0,0 +1,9 @@
+let person = prompt ("Please enter your name", "Por ejemplo, 'Dumbeldor'");
+
+function saludar(){
+  alert("Hi " + person);
+}
+
+function despedir(){
+  alert("Chau " + person);
+}
