1929103aa1cdcbb3d8b6b9bfbabad9d11f0a0581
SantiTru <santiagotrujillonavarro@gmail.com>
2023-10-06 19:32:36 +0200

    Commit

--- DesarrolloWebEntornoCliente/RepasoJavaScript/script.js
+++ DesarrolloWebEntornoCliente/RepasoJavaScript/script.js
@@ -1,3 +1,33 @@
+/*
+PRUEBA DE PROMPT Y MOSTRAR POR CONSOLA
 alert("Hola mundo");
 let entrada_datos = prompt("Ingrese su nombre");
 console.log("te llamas: " + entrada_datos);
+console.log(`te llamas: ${entrada_datos}`);
+*/
+//Variables
+
+let saludar = "Hola";
+if (true) {
+  saludar = "Adios mundo cruel";
+  console.log(saludar);
+}
+console.log(saludar);
+
+const numeroPi = 3.14;
+console.log(numeroPi);
+
+/*
+NO ES POSIBLE ASIGNAR VARIABLES A CONSTANTES
+numeroPi = 3.15;
+console.log(numeroPi);
+*/
+
+let docente = "Rocio Lopez";
+let curso = "DAW";
+let docente_curso = docente + " profesora de " + curso;
+console.log(docente_curso);
+
+let docente_curso2 = `${docente} profesora de ${curso}`;
+console.log(docente_curso2);
+
