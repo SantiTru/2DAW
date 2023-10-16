0b6d9ab436dab925ae5e2e621f64a988c4c37dce
SantiTru <santiagotrujillonavarro@gmail.com>
2023-10-16 18:37:12 +0200

    Commit

---
+++ DesarrolloWebEntornoCliente/JavaScript/PrimerProyecto/Script.js
@@ -0,0 +1,36 @@
+let tiempoRestante;
+let intervalId;
+
+document.getElementById("encenderAlarma").addEventListener("click", function () {
+    const inputTiempo = document.getElementById("tiempoSegundos").value;
+    if (isNaN(inputTiempo) || inputTiempo <= 0) {
+        alert("Por favor, ingresa un valor válido en segundos.");
+        return;
+    }
+    tiempoRestante = inputTiempo;
+    startCountdown();
+});
+
+document.getElementById("apagarAlarma").addEventListener("click", function () {
+    clearInterval(intervalId);
+    document.getElementById("alarma").pause();
+    document.getElementById("estadoAlarma").textContent = "Alarma apagada";
+    document.getElementById("encenderAlarma").style.display = "inline";
+    document.getElementById("apagarAlarma").style.display = "none";
+});
+
+function startCountdown() {
+    document.getElementById("encenderAlarma").style.display = "none";
+    document.getElementById("apagarAlarma").style.display = "inline";
+    document.getElementById("estadoAlarma").textContent = "Alarma encendida";
+
+    intervalId = setInterval(function () {
+        tiempoRestante--;
+        if (tiempoRestante <= 0) {
+            clearInterval(intervalId);
+            document.getElementById("alarma").play();
+            document.getElementById("estadoAlarma").textContent = "¡Alarma sonando!";
+            document.getElementById("apagarAlarma").style.display = "inline";
+        }
+    }, 1000);
+}
