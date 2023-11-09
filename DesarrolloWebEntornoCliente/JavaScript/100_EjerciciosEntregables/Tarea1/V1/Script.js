28555777d3b56936f68894c9e02b0de82011e4cb
SantiTru <santiagotrujillonavarro@gmail.com>
2023-11-09 03:23:11 +0100

    Commit

--- DesarrolloWebEntornoCliente/JavaScript/100_EjerciciosEntregables/Tarea1/V1/Script.js
+++ DesarrolloWebEntornoCliente/JavaScript/100_EjerciciosEntregables/Tarea1/V1/Script.js
@@ -1,15 +1,18 @@
 // Este código se ejecuta cuando el documento HTML ha sido completamente cargado y está listo para interactuar con JavaScript.
 document.addEventListener("DOMContentLoaded", () => {
-  // Obtenemos una referencia al elemento con el ID "Consulta" (presumiblemente un botón) y lo almacenamos en la variable consultaButton.
+  // Obtenemos una referencia al elemento con el ID "Consulta" y lo almacenamos en la variable consultaButton.
   const consultaButton = document.getElementById("Consulta");
 
-  // Obtenemos una referencia al elemento con el ID "PalabraConsultada" (presumiblemente un campo de entrada de texto) y lo almacenamos en la variable palabraInput.
+  // Obtenemos una referencia al elemento con el ID "PalabraConsultada" y lo almacenamos en la variable palabraInput.
   const palabraInput = document.getElementById("PalabraConsultada");
 
-  // Obtenemos una referencia al elemento con el ID "resultado" (presumiblemente un contenedor para mostrar el resultado) y lo almacenamos en la variable resultadoDiv.
-  const resultadoDiv = document.getElementById("resultado");
+  // Obtenemos una referencia al elemento con el ID "resultadoTexto" y lo almacenamos en la variable resultadoTexto.
+  const resultadoTexto = document.getElementById("resultadoTexto");
 
-  // Obtenemos una referencia al elemento con el ID "resultadoContainer" (presumiblemente un contenedor para mostrar el resultado de la consulta) y lo almacenamos en la variable resultadoContainer.
+  // Obtenemos una referencia al elemento con el ID "imagenResultado" y lo almacenamos en la variable imagenResultado.
+  const imagenResultado = document.getElementById("imagenResultado");
+
+  // Obtenemos una referencia al elemento con el ID "resultadoContainer" y lo almacenamos en la variable resultadoContainer.
   const resultadoContainer = document.getElementById("resultadoContainer");
 
   // Agregamos un evento click al botón "Consulta".
@@ -17,66 +20,115 @@ document.addEventListener("DOMContentLoaded", () => {
     // Obtenemos el valor del campo de entrada de texto, eliminamos espacios en blanco al principio y al final, y lo convertimos a minúsculas.
     const palabraConsultada = palabraInput.value.trim().toLowerCase();
 
-    // Llamamos a la función obtenerSignificado y le pasamos la palabra consultada como argumento. El resultado se almacena en la variable significado.
-    let significado = obtenerSignificado(palabraConsultada);
+    // Llamamos a la función obtenerResultado y le pasamos la palabra consultada como argumento. El resultado se almacena en la variable resultado.
+    const resultado = obtenerResultado(palabraConsultada);
 
-    // Comprobamos si hay un significado para la palabra.
-    if (significado) {
-      // Si hay un significado, mostramos el contenedor resultado al cambiar su propiedad de estilo display a "block" (visible).
-      resultadoContainer.style.display = "block"; // Mostramos el contenedor
-      // Llenamos el contenido del div resultadoDiv con el significado obtenido y la palabra consultada.
-      resultadoDiv.innerHTML = `<strong>Significado de "${palabraConsultada}":</strong> ${significado}`;
-    } else {
-      // Si no hay un significado, ocultamos el contenedor resultado al cambiar su propiedad de estilo display a "none" (oculto).
-      resultadoContainer.style.display = "none"; // Ocultamos el contenedor
-    }
+    // Comprobamos si hay un resultado para la palabra.
+    if (resultado) {
+      // Si hay un resultado, mostramos el contenedor resultado al cambiar su propiedad de estilo display a "block" (visible).
+      resultadoContainer.style.display = "block"; // Muestra el contenedor
+
+      // Llenamos el contenido del elemento resultadoTexto con el texto del resultado obtenido.
+      resultadoTexto.innerHTML = resultado.frase; // Muestra la frase
+
+      // Configuramos la fuente de la imagen para que muestre la imagen correspondiente al resultado.
+      imagenResultado.src = resultado.imagen; // Muestra la imagen
+    } 
   });
 
-  // Definimos una función llamada obtenerSignificado que toma una palabra como argumento y devuelve su significado basado en una estructura switch-case.
-  function obtenerSignificado(palabra) {
+  // Definimos una función llamada obtenerResultado que toma una palabra como argumento y devuelve su resultado basado en una estructura switch-case.
+  function obtenerResultado(palabra) {
+    let frase;
+    let imagen;
+
+    // Utilizamos una estructura switch-case para determinar el resultado basado en la palabra consultada.
     switch (palabra) {
       case "achuchaera":
-        return "Expresión utilizada para referirse a un lugar estrecho o apretado.";
+        frase = "Expresión utilizada para referirse a una amistad entre dos personas.";
+        imagen = "./Img/Achuchaera.jpg";
+        break;
       case "alargar":
-        return "Llevar a alguien a algún sitio.";
+        frase = "Llevar a alguien a algún sitio.";
+        imagen = "./Img/Alargar.jpg";
+        break;
       case "aliquindoi":
-        return "Expresión malagueña que significa 'Estar atento a algo para aprender como se hace'. Viene de la expresión inglesa 'Look and do it.'";
+        frase ="Expresión malagueña que significa 'Estar atento a algo para aprender cómo se hace'. Viene de la expresión inglesa 'Look and do it.'";
+        imagen = "./Img/Aliquindoi.jpg";
+        break;
       case "alobao":
-        return "Persona despistada o en las nubes.";
+        frase = "Persona despistada o en las nubes.";
+        imagen = "./Img/Alobao.jpg";
+        break;
       case "chorraera":
-        return "Tobogan. Mobiliario tipico de parques infantiles.";
+        frase = "Tobogán. Mobiliario típico de parques infantiles.";
+        imagen = "./Img/Chorraera.jpg";
+        break;
       case "eonoe":
-        return "Expresión que se usa para confirmar un argumento. ¿Es, o no es así?";
+        frase = "Expresión que se usa para confirmar un argumento. ¿Es, o no es así?";
+        imagen = "./Img/EONo.jpg";
+        break;
       case "encalomao":
-        return "Acoplarse a algún plan.";
+        frase = "Acoplarse a algún plan.";
+        imagen = "./Img/Encalomao.jpg";
+        break;
       case "encogio":
-        return "Alguien que no es muy generoso.";
+        frase = "Alguien que no es muy generoso.";
+        imagen = "./Img/Encogio.jpg";
+        break;
       case "escopetao":
-        return "Expresión utilizada para referirse a alguien que va con mucha prisa.";
+        frase = "Expresión utilizada para referirse a alguien que va con mucha prisa.";
+        imagen = "./Img/Escopetao.jpg";
+        break;
       case "farola":
-        return "Mandar a alguien a la Farola es el equivalente Malagueño a mandarlo a paseo.";
+        frase = "Mandar a alguien a la Farola es el equivalente Malagueño a mandarlo a paseo.";
+        imagen = "./Img/farola.jpg";
+        break;
       case "fite":
-        return "Fijate.";
+        frase = "Fíjate.";
+        imagen = "./Img/Fite.jpg";
+        break;
       case "guarnio":
-        return "Estar extremadamente cansado.";
+        frase = "Estar extremadamente cansado.";
+        imagen = "./Img/guarnio.jpg";
+        break;
       case "guarrito":
-        return "Taladro. Viene de la primera marca que se hizo con el mercado Malagueño de esta herramienta. La marca Warrington.";
+        frase = "Taladro. Viene de la primera marca que se hizo con el mercado Malagueño de esta herramienta. La marca Warrington.";
+        imagen = "./Img/Guarrito.jpg";
+        break;
       case "jaba":
-        return "Pie. Da lugar a la expresión Pedaso de jaba, dicha cuando alguien tiene un pie muy grande.";
+        frase = "Pie. Da lugar a la expresión 'Pedaso de jaba', dicha cuando alguien tiene un pie muy grande.";
+        imagen = "./Img/Jaba.jpg";
+        break;
       case "merdellon":
-        return "Persona con poca educación. Viene de la expresión en francés 'merde de gens'";
+        frase = "Persona con poca educación. Viene de la expresión en francés 'merde de gens'.";
+        imagen = "./Img/Merdellon.jpg";
+        break;
       case "pecha":
-        return "Cantidad grande de alguna cosa.";
+        frase = "Cantidad grande de alguna cosa.";
+        imagen = "./Img/Pecha.jpg";
+        break;
       case "perita":
-        return "Expresión que se utiliza para decir que algo esta guay.";
+        frase = "Expresión que se utiliza para decir que algo está guay, divertido o es de buena calidad.";
+        imagen = "./Img/Perita.jpg";
+        break;
       case "piarda":
-        return "Faltar a clase.";
+        frase = "Faltar a clase.";
+        imagen = "./Img/Piarda.jpg";
+        break;
       case "pila de ente":
-        return "Muchas personas.";
+        frase = "Muchas personas.";
+        imagen = "./Img/piladeEnte.jpg";
+        break;
       case "torroles":
-        return "La forma popular de conocer al pueblo de Torremolinos.";
-      default:
-        return "La palabra no se encuentra en el diccionario Malagueño lamentablemente.";
+        frase = "La forma popular de conocer al pueblo de Torremolinos.";
+        imagen = "./Img/ATorroles.jpg";
+        break;
+        default:
+          // Si la palabra no coincide con ningún caso, retornamos null para indicar que no hay un resultado.
+          return null;
+      }
+  
+      // Devolvemos un objeto con la frase y la imagen correspondientes a la palabra consultada.
+      return { frase, imagen };
     }
-  }
-});
+  });
