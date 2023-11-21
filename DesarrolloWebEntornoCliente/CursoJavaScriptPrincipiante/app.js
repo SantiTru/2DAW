b3f6ed9fa8ff4ff9dbc4b72e72413aa224628181
SantiTru <santiagotrujillonavarro@gmail.com>
2023-11-21 17:55:31 +0100

    Commit

---
+++ DesarrolloWebEntornoCliente/CursoJavaScriptPrincipiante/app.js
@@ -0,0 +1,8 @@
+alert('Hello World');
+
+console.log('log message');
+console.info('info message');
+console.warn('warning message');
+console.error('error message');
+debugger
+console.log('after debugger');
