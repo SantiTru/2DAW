7e73abfe1fdebcbbff740c551988bbc20bd30f63
SantiTru <santiagotrujillonavarro@gmail.com>
2023-12-09 22:14:37 +0100

    Commit

--- DespliegueAplicaciones/Login_PDO/index.php
+++ DespliegueAplicaciones/Login_PDO/index.php
@@ -1,4 +1,15 @@
 <?php
+
+// Establecer el tiempo de expiración de la cookie
+$expire = time() + 5; // 5 segundos
+
+// Crear la cookie
+setcookie('Oreo', 'dobleCrema', $expire);
+
+// Redirigir a la misma página
+header('Location: ' . $_SERVER['PHP_SELF']);
+exit;
+
 session_start();
 
 if (isset($_SESSION['usuario'])) {
