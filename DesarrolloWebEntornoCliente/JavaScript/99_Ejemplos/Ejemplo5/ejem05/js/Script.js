d755c6cf9a24f6dde875038d1a8bc7d92cf6e59e
Santiago Trujillo Navarro <117385307+SantiTru@users.noreply.github.com>
2024-01-02 19:37:45 +0100

    Delete DesarrolloWebEntornoCliente/JavaScript/99_Ejemplos directory

--- DesarrolloWebEntornoCliente/JavaScript/99_Ejemplos/Ejemplo5/ejem05/js/Script.js
+++
@@ -1,5 +0,0 @@
-let person = prompt ("Please enter your name", "Por ejemplo, 'Rubeus'");
-let apellido = prompt ("Please enter your surname", "Por ejemplo, 'Hagrid'");
-
-alert("Hola " + person + "  " + apellido + ", disfruta la web.");
-
